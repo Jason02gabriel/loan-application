bc510cdc29c93420713ff15bf2b449fd468ed8ba
Jason_Gabriel <j.gabrielaraujodasilva@gmail.com>
2024-05-14 08:43:03 -0300

    feat: Customer created

---
+++ src/main/java/com/biel/loans/domain/Customer.java
@@ -0,0 +1,22 @@
+package com.biel.loans.domain;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+
+    private Integer age;
+    private String cpf;
+    private String name;
+    private Double income;
+    private String location;
+
+    public Customer(Integer age, String cpf, String name, Double income, String location) {
+        this.age = age;
+        this.cpf = cpf;
+        this.name = name;
+        this.income = income;
+        this.location = location;
+    }
+
+}
